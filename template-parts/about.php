c0683c3c816de953eb490c02920514aa58f31b53
val <vale.dorval@gmail.com>
2020-11-05 18:56:37 -0500

    responsive

--- template-parts/about.php
+++ template-parts/about.php
@@ -13,38 +13,52 @@
      <div class="team__items flex">
           <article class="team__items--box">
                <img class="u-margin-bottom-small" src="<?php echo get_template_directory_uri(); ?>/img/val.jpg" alt="image Valérie">
-               <h4 class="heading-quaternary u-margin-bottom-small">Valérie Dorval</h4>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, turpis morbi viverra metus feugiat aliquet ante sed. Enim metus risus ac tincidunt diam. Neque, amet pulvinar nunc elementum elit quisque consectetur. Enim risus libero potenti augue vulputate auctor nunc. Leo nam faucibus in a erat a. Iaculis egestas ipsum ac aliquet nunc.</p>
+               <div class="team--container">
+                    <h4 class="heading-quaternary u-margin-bottom-small">Valérie Dorval</h4>
+                    <p>Avant-gardiste et pragmatique, Valérie possède une expérience solide qui lui offre une forte capacité à envisager toutes les situations dans leur ensemble sans négliger pour autant les détails. Sa devise: "Être à votre service pour que votre application Internet vous convienne à la perfection!". Consciencieuse et méthodique, elle permet de faire aboutir au mieux les projets qui sont confiés à l'équipe.</p>
+               </div>
           </article>
           <article class="team__items--box">
                <img class="u-margin-bottom-small" src="<?php echo get_template_directory_uri(); ?>/img/bianka.jpg" alt="image Bianka">
-               <h4 class="heading-quaternary u-margin-bottom-small">Bianka Hallé</h4>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, turpis morbi viverra metus feugiat aliquet ante sed. Enim metus risus ac tincidunt diam. Neque, amet pulvinar nunc elementum elit quisque consectetur. Enim risus libero potenti augue vulputate auctor nunc. Leo nam faucibus in a erat a. Iaculis egestas ipsum ac aliquet nunc.</p>
+               <div class="team--container">
+                    <h4 class="heading-quaternary u-margin-bottom-small">Bianka Hallé</h4>
+                    <p>Organisée et structurée, Bianka s’assure du bon déroulement de chaque projet et du respect des délais. Elle apporte à l'équipe son point de vue rafraîchissant et guide les projets avec leadership. Elle coordonne les échanges et le partage d'informations entre le client et l’équipe. Elle adore aider les autres et, avec son attitude positive, contribue activement au bon climat de travail.</p>
+               </div>
           </article>
           <article class="team__items--box">
                <img class="u-margin-bottom-small" src="<?php echo get_template_directory_uri(); ?>/img/j-f.jpg" alt="image Jean-Francis">
-               <h4 class="heading-quaternary u-margin-bottom-small">Jean-Francis-Gautier Konan</h4>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, turpis morbi viverra metus feugiat aliquet ante sed. Enim metus risus ac tincidunt diam. Neque, amet pulvinar nunc elementum elit quisque consectetur. Enim risus libero potenti augue vulputate auctor nunc. Leo nam faucibus in a erat a. Iaculis egestas ipsum ac aliquet nunc.</p>
+               <div class="team--container">
+                    <h4 class="heading-quaternary u-margin-bottom-small">Jean-Francis-Gautier Konan</h4>
+                    <p>Développeur web full-stack. Avec son expérience dans des domaines variés, il est un véritable pilier pour l’équipe. Il aime contribuer aux nouveaux projets avec ses idées novatrices et pour lui, chaque obstacle est une une nouvelle opportunité à saisir.</p>
+               </div>
           </article>
           <article class="team__items--box">
                <img class="u-margin-bottom-small" src="<?php echo get_template_directory_uri(); ?>/img/marie.jpg" alt="image Marie-Camille">
-               <h4 class="heading-quaternary u-margin-bottom-small">Marie-Camille Laberge</h4>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, turpis morbi viverra metus feugiat aliquet ante sed. Enim metus risus ac tincidunt diam. Neque, amet pulvinar nunc elementum elit quisque consectetur. Enim risus libero potenti augue vulputate auctor nunc. Leo nam faucibus in a erat a. Iaculis egestas ipsum ac aliquet nunc.</p>
+               <div class="team--container">
+                    <h4 class="heading-quaternary u-margin-bottom-small">Marie-Camille Laberge</h4>
+                    <p>Graphiste et développeuse web, Marie-Camille aime créer des interfaces minimalistes et accessibles. Son expérience dans le web et l'imprimé l'on amenée à placer l'utilisateur au centre de ses réflexions et de ses recherches. Allergique au superflus et aux tendances, elle cherche toujours des solutions simples et durables pour régler des problèmes complexes.</p>
+               </div>
           </article>
           <article class="team__items--box">
                <img class="u-margin-bottom-small" src="<?php echo get_template_directory_uri(); ?>/img/theo.jpg" alt="image Théodore">
-               <h4 class="heading-quaternary u-margin-bottom-small">Théodore Leuwat-Ngogang</h4>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, turpis morbi viverra metus feugiat aliquet ante sed. Enim metus risus ac tincidunt diam. Neque, amet pulvinar nunc elementum elit quisque consectetur. Enim risus libero potenti augue vulputate auctor nunc. Leo nam faucibus in a erat a. Iaculis egestas ipsum ac aliquet nunc.</p>
+               <div class="team--container">
+                    <h4 class="heading-quaternary u-margin-bottom-small">Théodore Leuwat-Ngogang</h4>
+                    <p>Développeur Web full-stack, Théodore Leuwat apporte à chaque projet son expérience client et ses aptitudes acquises au fils des années. Il recherche avant tout la compatibilité entre les besoins et les applications utilisées. En contribuant à la fois à la conception des interfaces et au développement des applications, son objectif est de satisfaire et de dépasser les attentes des clients.</p>
+               </div>
           </article>
           <article class="team__items--box">
                <img class="u-margin-bottom-small" src="<?php echo get_template_directory_uri(); ?>/img/safa.jpg" alt="image Safa">
-               <h4 class="heading-quaternary u-margin-bottom-small">Safa Sammouda</h4>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, turpis morbi viverra metus feugiat aliquet ante sed. Enim metus risus ac tincidunt diam. Neque, amet pulvinar nunc elementum elit quisque consectetur. Enim risus libero potenti augue vulputate auctor nunc. Leo nam faucibus in a erat a. Iaculis egestas ipsum ac aliquet nunc.</p>
+               <div class="team--container">
+                    <h4 class="heading-quaternary u-margin-bottom-small">Safa Sammouda</h4>
+                    <p>Safa possède l’expertise technique en design et en développement d'applications Web. Après plusieurs années en tant que programmeur, ses atouts multidisciplinaires lui permettent de s'occuper de toutes les phases et les détails du développement de votre application , afin que ce processus se traduit par le succès et soit réalisé avec les dernières technologies.</p>
+               </div>
           </article>
           <article class="team__items--box">
                <img class="u-margin-bottom-small" src="<?php echo get_template_directory_uri(); ?>/img/mahdi.jpg" alt="image Mahdi">
-               <h4 class="heading-quaternary u-margin-bottom-small">Mahdi Touazi</h4>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, turpis morbi viverra metus feugiat aliquet ante sed. Enim metus risus ac tincidunt diam. Neque, amet pulvinar nunc elementum elit quisque consectetur. Enim risus libero potenti augue vulputate auctor nunc. Leo nam faucibus in a erat a. Iaculis egestas ipsum ac aliquet nunc.</p>
+               <div class="team--container">
+                    <h4 class="heading-quaternary u-margin-bottom-small">Mahdi Touazi</h4>
+                    <p>Développeur web full-stack et gestionnaire des opérations. Avec l'expérience dans le domaine de la gestion, le développement web devient plus organiser et gérer d'une manière unique et objective. Il est apte à apporter des solutions adaptées aux besoins du client et son équipe de travail. Il est focalisé sur la création d'un code irréprochable.</p>
+               </div>
           </article>
      </div>
 </section>
